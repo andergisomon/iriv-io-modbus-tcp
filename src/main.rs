4d4baf586061be7139072e7f271e535ba4706852
andergisomon <volintine_21001524@utp.edu.my>
2025-10-28 14:17:07 +0800

    More updates

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@
 use defmt::*;
 use embassy_executor::Spawner;
 use embassy_futures::yield_now;
-use embassy_net::udp::{PacketMetadata, UdpSocket};
 use embassy_net::{Stack, StackResources};
 use embassy_net_wiznet::chip::W5500;
 use embassy_net_wiznet::*;
@@ -12,9 +11,11 @@ use embassy_rp::clocks::RoscRng;
 use embassy_rp::gpio::{Input, Level, Output, Pull};
 use embassy_rp::peripherals::SPI0;
 use embassy_rp::spi::{Async, Config as SpiConfig, Spi};
-use embassy_time::Delay;
+use embassy_time::{Delay, Duration};
 use embedded_hal_bus::spi::ExclusiveDevice;
+use embedded_io_async::Write;
 use static_cell::StaticCell;
+use modbus_core::*;
 
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
