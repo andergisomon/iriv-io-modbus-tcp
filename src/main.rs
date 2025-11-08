a6f890368e8d837833342e96769bb23c127e470b
andergisomon <volintine_21001524@utp.edu.my>
2025-11-08 15:24:56 +0800

    DI/O callbacks

--- src/main.rs
+++ src/main.rs
@@ -90,6 +90,6 @@ async fn main(spawner: Spawner) {
             warn!("accept error: {:?}", e);
             continue;
         }
-        transact_client(&mut buf, socket).await;
+        transact_client(&mut buf, &mut iriv_hal, socket).await;
     }
 }
--- src/modbus.rs
+++ src/modbus.rs
@@ -1,20 +1,15 @@
 use defmt::*;
 use embassy_net::tcp::TcpSocket;
+use embassy_rp::gpio::{Input, Level};
 use modbus_core::tcp::{RequestAdu, ResponseAdu};
 use modbus_core::tcp::server::{self, decode_request, encode_response};
-use modbus_core::{Data, FunctionCode, Request, RequestPdu, Response};
-
-#[derive(PartialEq, Eq, Debug, Format, Clone)]
-pub enum Error {
-    Modbus,
-    Transport,
-    Timeout,
-}
+use modbus_core::{Error, Data, FunctionCode, Request, RequestPdu, Response};
+use crate::Hal;
 
 /// Services client reads and writes
 /// TCP socket timeout is set in main()
 /// modbus-core has yet to implement modbus exception responses
-pub async fn transact_client<'r>(buf: &mut [u8], socket: TcpSocket<'static>) -> Result<Response<'r>> {
+pub async fn transact_client<'r>(buf: &mut [u8], hal: &mut Hal, socket: TcpSocket<'static>) -> Result<(), Error> {
 
     loop {
         let n = match socket.read(&mut buf).await {
@@ -31,9 +26,7 @@ pub async fn transact_client<'r>(buf: &mut [u8], socket: TcpSocket<'static>) -> 
         info!("received {:?}", &buf[..n]);
     }
 
-    let req = modbus_core::tcp::server::decode_request(buf)
-        .map_err(|_| Error::Transport)?
-        .ok_or(Error::Modbus)?;
+    let req = modbus_core::tcp::server::decode_request(buf)?.unwrap();
 
     // handle what kind of modbus request it is
     // just use match statements here to handle client read/writes
@@ -45,7 +38,27 @@ pub async fn transact_client<'r>(buf: &mut [u8], socket: TcpSocket<'static>) -> 
     let resp = ResponseAdu {hdr: header, pdu: resp_data};
 
     // resp_tcp_buf is the TCP datagram
-    let n = encode_response(resp, resp_tcp_buf) // form a TCP buffer from the response
+    let n = encode_response(resp, resp_tcp_buf); // form a TCP buffer from the response
 
     // once all is done, call write on the socket and flush
+
+    Ok(())
+}
+
+pub fn din_get_cb(pin: usize, hal: &mut Hal) -> u8 {
+    let di_hdl = &mut hal.din;
+    let res = match di_hdl[pin].get_level() {
+        Level::Low => 0,
+        Level::High => 1,
+    };
+    res
+}
+
+pub fn dout_set_cb(pin: usize, hal: &mut Hal, val: bool) {
+    let dout_hdl = &mut hal.dout;
+    let val = match val {
+        false => Level::Low,
+        true => Level::High
+    };
+    dout_hdl[pin].set_level(val);
 }
