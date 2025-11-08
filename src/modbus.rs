948e988c0958fb4111673411a17a32e6e0777db8
andergisomon <volintine_21001524@utp.edu.my>
2025-11-08 19:47:27 +0800

    handling modbus requests

--- src/modbus.rs
+++ src/modbus.rs
@@ -3,7 +3,7 @@ use embassy_net::tcp::TcpSocket;
 use embassy_rp::gpio::{Level};
 use modbus_core::tcp::{RequestAdu, ResponseAdu};
 use modbus_core::tcp::server::{decode_request, encode_response};
-use modbus_core::{Error, Request};
+use modbus_core::{Coils, Data, Error, Request, RequestPdu, Response, ResponsePdu};
 use crate::Io;
 
 /// Register addresses
@@ -12,6 +12,7 @@ const DO0_ADDR: u16 = 0x0100; /// Digital Output 0
 const DO1_ADDR: u16 = 0x0101; /// Digital Output 1
 const DO2_ADDR: u16 = 0x0102; /// Digital Output 2
 const DO3_ADDR: u16 = 0x0103; /// Digital Output 3
+const MAX_NUMBER_OF_COILS: usize = 4;
 
 /// Contacts (0x1) - Read-only
 const DI0_ADDR:  u16 = 0x0000; /// Digital Input 0
@@ -73,16 +74,48 @@ pub async fn transact_client(buf: &mut [u8], hal: &mut Io, socket: TcpSocket<'_>
     // handle what kind of modbus request it is
     // just use match statements here to handle client read/writes
     let RequestAdu {hdr: header, pdu: req_data} = req;
-    match req_data {
-        Request::ReadCoils(addr, quantity),
-    }
 
-    // copy MBAP header, put data to service request
-    // resp_data need to be populated by a helper function that relays data from the DI/O and AI
-    // just use match statements here to handle client read/writes
-    let resp = ResponseAdu {hdr: header, pdu: resp_data};
+    let mut resp_data;
+    let mut resp;
+    let target_buf = &mut [0u8; MAX_NUMBER_OF_COILS];
+
+    match req_data {
+        RequestPdu(Request::ReadCoils(addr, quantity)) => {
+            let bools = &mut [false; MAX_NUMBER_OF_COILS];
+            for i in 0..quantity as usize {
+                bools[i] = din_get_cb(i, &hal);
+            }
+
+            match addr {
+                DO0_ADDR => ,
+                DO1_ADDR => ,
+                DO2_ADDR => ,
+                DO3_ADDR => ,
+            }
+
+            let coils = Coils::from_bools(bools, target_buf).unwrap();
+            resp_data = ResponsePdu(Ok(Response::ReadCoils(coils)));
+            resp = ResponseAdu {hdr: header, pdu: resp_data}; // copy MBAP header, put data to service request
+        },
+        RequestPdu(Request::ReadInputRegisters(addr, quantity)) => {
+
+            // handle requests here
+
+            resp_data = ResponsePdu(Ok(Response::ReadInputRegisters(Data::from_words(&[0], target_buf).unwrap())));
+            resp = ResponseAdu {hdr: header, pdu: resp_data}; // copy MBAP header, put data to service request
+        },
+        RequestPdu(Request::WriteSingleCoil(addr, val)) => {
+
+            // handle requests here
+
+            resp_data = ResponsePdu(Ok(Response::WriteSingleCoil(addr)));
+            resp = ResponseAdu {hdr: header, pdu: resp_data}; // copy MBAP header, put data to service request
+        },
+        _ => ()
+    };
 
     // resp_tcp_buf is the TCP datagram
+    let resp_tcp_buf = &mut [0u8; 4096];
     let n = encode_response(resp, resp_tcp_buf); // form a TCP buffer from the response
 
     // once all is done, call write on the socket and flush
@@ -90,8 +123,8 @@ pub async fn transact_client(buf: &mut [u8], hal: &mut Io, socket: TcpSocket<'_>
     Ok(())
 }
 
-pub fn din_get_cb(pin: usize, hal: &mut Io) -> bool {
-    let di_hdl = &mut hal.din;
+pub fn din_get_cb(pin: usize, hal: &Io) -> bool {
+    let di_hdl = &hal.din;
     let res = match di_hdl[pin].get_level() {
         Level::Low => false,
         Level::High => true,
